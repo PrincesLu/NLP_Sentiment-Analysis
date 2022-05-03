61bcffb0afaf7922a0d5c499a1b75e94ea7e1067
PrincesLu <94855929+PrincesLu@users.noreply.github.com>
2022-05-03 23:03:55 +0100

    Update part1.py

--- Classification_HateSpeach/part1.py
+++ Classification_HateSpeach/part1.py
@@ -1,1 +1,38 @@
+#general packages for data manipulation
+import os
+import pandas as pd
+import numpy as np
+#visualizations
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+#consistent sized plot 
+from pylab import rcParams
+rcParams['figure.figsize']=12,5
+rcParams['axes.labelsize']=12
+rcParams['xtick.labelsize']=12
+rcParams['ytick.labelsize']=12
+#handle the warnings in the code
+import warnings
+warnings.filterwarnings(action='ignore',category=DeprecationWarning)
+warnings.filterwarnings(action='ignore',category=FutureWarning)
+#text preprocessing libraries
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import WordPunctTokenizer
+from nltk.tokenize import TweetTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+#import texthero
+#import texthero as hero
+#regular expressions
+import re
+#display pandas dataframe columns 
+pd.options.display.max_columns = None
 
+#load the csv file as a pandas dataframe
+#ISO-8859-1
+tweet = pd.read_csv('/kaggle/input/twitter-hate-speech/TwitterHate.csv',delimiter=',',engine='python',encoding='utf-8-sig')
+tweet.head()
